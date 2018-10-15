f8a873ddf94b1aff5f70605efa462228901974e5
masato <masato@m-mbp.local>
2018-10-15 13:09:52 +0800

    initial commit

--- Facebook Messenger/AppDelegate.swift
+++ Facebook Messenger/AppDelegate.swift
@@ -15,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+
+        let layout = UICollectionViewFlowLayout()
+        let friendsController = FriendsController(collectionViewLayout: layout)
+        window?.rootViewController = UINavigationController(rootViewController: friendsController)
         // Override point for customization after application launch.
         return true
     }
--- Facebook Messenger/ViewController.swift
+++ Facebook Messenger/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    private let cellID = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView?.backgroundColor = UIColor.red
+        collectionView?.alwaysBounceVertical = true
+
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellID)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+
+    }
+
+    func  collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
     }
 
 
+
+}
+
+    class FriendCell: UICollectionViewCell {
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupViews()
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        func setupViews() {
+            backgroundColor = UIColor.blue
+
+        }
 }
 
